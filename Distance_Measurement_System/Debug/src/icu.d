3493f1aa3710de855d7d0bfaf3657dfff7f0d6e1
Marwan Fawzy Shahat Mahmoud <105066899+ArabianHindi@users.noreply.github.com>
2023-06-27 18:51:30 +0300

    Delete Distance_Measurement_System/Debug directory

--- Distance_Measurement_System/Debug/src/icu.d
+++
@@ -1,8 +0,0 @@
-src/icu.o src/icu.o: ../src/icu.c ../src/icu.h ../src/std_types.h \
-  ../src/Macros.h
-
-../src/icu.h:
-
-../src/std_types.h:
-
-../src/Macros.h:
--- Distance_Measurement_System/Debug/src/lcd.d
+++
@@ -1,8 +0,0 @@
-src/lcd.o src/lcd.o: ../src/lcd.c ../src/lcd.h ../src/gpio.h \
-  ../src/std_types.h
-
-../src/lcd.h:
-
-../src/gpio.h:
-
-../src/std_types.h:
